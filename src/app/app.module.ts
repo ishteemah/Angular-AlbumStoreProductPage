916a79d47c1476a532cb6d1b730fcbf355582772
ishteemah <ishtimah@gmail.com>
2019-10-25 13:14:20 -0700

    setup product-description component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
-import { RouterModule, Routes } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from "./app.component";
-import { ProductPageComponent } from "./product-page/product-page.component";
-import { ProductDescriptionComponent } from "./product-description/product-description.component";
-import { ProductService } from "./product.service";
-import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
-import { ProductListComponent } from "./product-list/product-list.component";
-
-const appRoutes: Routes = [
-  { path: "products", component: ProductListComponent },
-  { path: "product/:id", component: ProductPageComponent },
-  { path: "", redirectTo: "products", pathMatch: "full" }
-];
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent,
-    ProductTracklistingComponent,
-    ProductListComponent
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes)
+    HttpModule
   ],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { ProductService } from "../product.service";
-import { Album } from "../album";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-product-description",
-  templateUrl: "./product-description.component.html",
-  styleUrls: ["./product-description.component.css"]
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo: Album;
-  constructor(private _productService: ProductService) {}
+
+  constructor() { }
 
   ngOnInit() {
-    this._productService
-      .getAlbum(1)
-      .subscribe(response => (this.albumInfo = response));
   }
+
 }
